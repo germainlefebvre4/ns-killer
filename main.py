a515b3ce6f9156a619cc0848496d663f8932a573
Germain Lefebvre <glefebvre@ineat.fr>
2019-12-17 23:50:52 +0100

    feat(config): Add python config way

--- main.py
+++ main.py
@@ -5,26 +5,42 @@ import datetime
 import os
 import time
 from ast import literal_eval
+import yaml
 
-PYTHON_LOOP_FREQUENCY = int(os.environ.get('PYTHON_LOOP_FREQUENCY'))
-NS_PATTERN = os.environ.get('NS_PATTERN')
-NS_RENTENTION_KIND = os.environ.get('NS_RENTENTION_KIND')
-NS_RENTENTION_TIME = int(os.environ.get('NS_RENTENTION_TIME'))
+# Load config file
+with open("ns-killer.conf", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+# Fulfill config params with default values
 
-ns_exlude = ['kube-system']
-
-#k8s_ns = os.system('kubectl get namespaces -o json')
+# Load cluster namespaces
+#with open('namespaces.json', 'r') as outfile:
+#    k8s_ns = json.load(outfile)
 k8s_ns = literal_eval(os.popen('kubectl get namespaces -o json').read())
-print(k8s_ns)
-print(type(k8s_ns))
+#print(k8s_ns)
+
+# Iterate on items
 for i in k8s_ns.get('items'):
     ns_name = i.get('metadata').get('name')
     ns_creationTimestamp = i.get('metadata').get('creationTimestamp')
     ns_creationTimestamp_date = datetime.datetime.strptime(ns_creationTimestamp, "%Y-%m-%dT%H:%M:%S%z")
-    if ns_name not in ns_exlude:
+    # Test if namespace is in exclude list
+    if ns_name not in cfg['namespace']['exclude']:
         date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
         ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date)
-        ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60)[0]
-        if ns_creation_ago_hours >= NS_RENTENTION_TIME:
-            print("{} | Killed namespace '{}' that lived for {} hours".format(date_now, ns_name, ns_creation_ago_hours))
+        if cfg['config']['retention']['kind'] == "hours":
+            ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60)[0]
+            if ns_creation_ago_hours >= cfg['config']['retention']['time']:
+                print("{} | Killed namespace '{}' that lived for {} hours".format(date_now, ns_name, ns_creation_ago_hours))
+        elif cfg['config']['retention']['kind'] == "days":
+            ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60*24)[0]
+            if ns_creation_ago_hours >= cfg['config']['retention']['time']:
+                print("{} | Killed namespace '{}' that lived for {} days".format(date_now, ns_name, ns_creation_ago_hours))
+        elif cfg['config']['retention']['kind'] == "weeks":
+            ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60*24*7)[0]
+            if ns_creation_ago_hours >= cfg['config']['retention']['time']:
+                print("{} | Killed namespace '{}' that lived for {} weeks".format(date_now, ns_name, ns_creation_ago_hours))
+        elif cfg['config']['retention']['kind'] == "months":
+            ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60*24*30)[0]
+            if ns_creation_ago_hours >= cfg['config']['retention']['time']:
+                print("{} | Killed namespace '{}' that lived for {} months".format(date_now, ns_name, ns_creation_ago_hours))
 
