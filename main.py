eca6b092c762101512d694d19ffbeae21e5b07e7
Germain Lefebvre <glefebvre@ineat.fr>
2019-12-17 17:23:11 +0100

    feat(config): Add kubernetes ConfigMap

--- main.py
+++ main.py
@@ -13,20 +13,18 @@ NS_RENTENTION_TIME = int(os.environ.get('NS_RENTENTION_TIME'))
 
 ns_exlude = ['kube-system']
 
-while True:
-    #k8s_ns = os.system('kubectl get namespaces -o json')
-    k8s_ns = literal_eval(os.popen('kubectl get namespaces -o json').read())
-    print(k8s_ns)
-    print(type(k8s_ns))
-    for i in k8s_ns.get('items'):
-        ns_name = i.get('metadata').get('name')
-        ns_creationTimestamp = i.get('metadata').get('creationTimestamp')
-        ns_creationTimestamp_date = datetime.datetime.strptime(ns_creationTimestamp, "%Y-%m-%dT%H:%M:%S%z")
-        if ns_name not in ns_exlude:
-            date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
-            ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date)
-            ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60)[0]
-            if ns_creation_ago_hours >= NS_RENTENTION_TIME:
-                print("{} | Killed namespace '{}' that lived for {} hours".format(date_now, ns_name, ns_creation_ago_hours))
-    time.sleep(PYTHON_LOOP_FREQUENCY)
+#k8s_ns = os.system('kubectl get namespaces -o json')
+k8s_ns = literal_eval(os.popen('kubectl get namespaces -o json').read())
+print(k8s_ns)
+print(type(k8s_ns))
+for i in k8s_ns.get('items'):
+    ns_name = i.get('metadata').get('name')
+    ns_creationTimestamp = i.get('metadata').get('creationTimestamp')
+    ns_creationTimestamp_date = datetime.datetime.strptime(ns_creationTimestamp, "%Y-%m-%dT%H:%M:%S%z")
+    if ns_name not in ns_exlude:
+        date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
+        ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date)
+        ns_creation_ago_hours = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, 60)[0]
+        if ns_creation_ago_hours >= NS_RENTENTION_TIME:
+            print("{} | Killed namespace '{}' that lived for {} hours".format(date_now, ns_name, ns_creation_ago_hours))
 
