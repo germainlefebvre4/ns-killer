a4b08303928d561650538a34bf4a1ea0aa574942
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2020-09-01 00:22:06 +0200

    fix: Syntax and datetime calls

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import json
-from datetime import timedelta, timezone
-import datetime
+from datetime import datetime, timedelta, timezone
 import os
 import time
 from ast import literal_eval
@@ -19,23 +18,24 @@ with open("/etc/config/ns-killer.conf", 'r') as ymlfile:
 k8s_ns = literal_eval(os.popen('kubectl get namespaces -o json').read())
 #print(k8s_ns)
 
-# time divider units in dict
+# Time divider units in dict
 time_divider_units = {
-    'minutes' : 60,
-    'hours' : 60 * 60,
-    'days' : 60 * 60 * 24,
-    'weeks' : 60 * 60 * 24 * 7,
-    'months' : 60 * 60 * 24 * 30
+    'minutes': 60,
+    'hours': 60 * 60,
+    'days': 60 * 60 * 24,
+    'weeks': 60 * 60 * 24 * 7,
+    'months': 60 * 60 * 24 * 30
 }
 
 def delete_ns(ns_name, ns_creationTimestamp_date):
-    date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
-    ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date
+    date_now = datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
+    print(datetime.now(timezone.utc), ns_creationTimestamp_date)
+    ns_creation_ago = (datetime.now(timezone.utc) - ns_creationTimestamp_date)
     # Define the time unit and calculte the time divider
-    time_divider_unit = time_divider_units[cfg['config']['retention']['kind']
+    time_divider_unit = time_divider_units.get(cfg['config']['retention']['kind'])
     ns_creation_ago_time = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, time_divider_unit)[0]
     if ns_creation_ago_time >= cfg['config']['retention']['time']:
-        os.system("kubectl delete namespace {}".format(ns_name))
+        #os.system("kubectl delete namespace {}".format(ns_name))
         print("{} | Killed namespace '{}' that lived for {} {}".format(date_now, ns_name, ns_creation_ago_time, cfg['config']['retention']['kind']))
 
 
@@ -44,7 +44,7 @@ print("Start iterating on namespaces")
 for i in k8s_ns.get('items'):
     ns_name = i.get('metadata').get('name')
     ns_creationTimestamp = i.get('metadata').get('creationTimestamp')
-    ns_creationTimestamp_date = datetime.datetime.strptime(ns_creationTimestamp, "%Y-%m-%dT%H:%M:%S%z")
+    ns_creationTimestamp_date = datetime.strptime(ns_creationTimestamp, "%Y-%m-%dT%H:%M:%S%z")
 
     if  len(cfg['namespace']['only']) == 0:
       # Test if namespace is in exclude list
