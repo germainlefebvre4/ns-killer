fecd67a8efaf90d5f7ba659e2abb8f56a1c38bda
bilgehannal <bilgehanl.03@gmail.com>
2020-08-31 09:50:05 +0300

    optimzed code for duplications

--- main.py
+++ main.py
@@ -19,6 +19,26 @@ with open("/etc/config/ns-killer.conf", 'r') as ymlfile:
 k8s_ns = literal_eval(os.popen('kubectl get namespaces -o json').read())
 #print(k8s_ns)
 
+# time divider units in dict
+time_divider_units = {
+    'minutes' : 60,
+    'hours' : 60 * 60,
+    'days' : 60 * 60 * 24,
+    'weeks' : 60 * 60 * 24 * 7,
+    'months' : 60 * 60 * 24 *30
+}
+
+def delete_ns(ns_name, ns_creationTimestamp_date):
+    date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
+    ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date
+    # Define the time unit and calculte the time divider
+    time_divider_unit = time_divider_units[cfg['config']['retention']['kind']
+    ns_creation_ago_time = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, time_divider_unit)[0]
+    if ns_creation_ago_time >= cfg['config']['retention']['time']:
+        os.system("kubectl delete namespace {}".format(ns_name))
+        print("{} | Killed namespace '{}' that lived for {} {}".format(date_now, ns_name, ns_creation_ago_time, cfg['config']['retention']['kind']))
+
+
 # Iterate on items
 print("Start iterating on namespaces")
 for i in k8s_ns.get('items'):
@@ -29,44 +49,8 @@ for i in k8s_ns.get('items'):
     if  len(cfg['namespace']['only']) == 0:
       # Test if namespace is in exclude list
       if ns_name not in cfg['namespace']['exclude']:
-          date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
-          ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date)
-  
-          # Define the time unit and calculte the time divider
-          if cfg['config']['retention']['kind'] == "minutes":
-              time_divider_unit = 60
-          elif cfg['config']['retention']['kind'] == "hours":
-              time_divider_unit = 60*60
-          elif cfg['config']['retention']['kind'] == "days":
-              time_divider_unit = 60*60*24
-          elif cfg['config']['retention']['kind'] == "weeks":
-              time_divider_unit = 60*60*24*7
-          elif cfg['config']['retention']['kind'] == "months":
-              time_divider_unit = 60*62*24*30
-  
-          ns_creation_ago_time = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, time_divider_unit)[0]
-          if ns_creation_ago_time >= cfg['config']['retention']['time']:
-              os.system("kubectl delete namespace {}".format(ns_name))
-              print("{} | Killed namespace '{}' that lived for {} {}".format(date_now, ns_name, ns_creation_ago_time, cfg['config']['retention']['kind']))
+          delete_ns(ns_name, ns_creationTimestamp_date)
     else:
         if ns_name in cfg['namespace']['only']:
-          date_now = datetime.datetime.now(timezone.utc).strftime('%Y-%M-%d %H:%M:%S')
-          ns_creation_ago = (datetime.datetime.now(timezone.utc) - ns_creationTimestamp_date)
-
-          # Define the time unit and calculte the time divider
-          if cfg['config']['retention']['kind'] == "minutes":
-              time_divider_unit = 60
-          elif cfg['config']['retention']['kind'] == "hours":
-              time_divider_unit = 60*60
-          elif cfg['config']['retention']['kind'] == "days":
-              time_divider_unit = 60*60*24
-          elif cfg['config']['retention']['kind'] == "weeks":
-              time_divider_unit = 60*60*24*7
-          elif cfg['config']['retention']['kind'] == "months":
-              time_divider_unit = 60*62*24*30
-
-          ns_creation_ago_time = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, time_divider_unit)[0]
-          if ns_creation_ago_time >= cfg['config']['retention']['time']:
-              os.system("kubectl delete namespace {}".format(ns_name))
-              print("{} | Killed namespace '{}' that lived for {} {}".format(date_now, ns_name, ns_creation_ago_time, cfg['config']['retention']['kind']))
+          delete_ns(ns_name, ns_creationTimestamp_date)
 
