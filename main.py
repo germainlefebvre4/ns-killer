c92786982fbafdf3dd9b72acc2a26db81928d929
mderacc <mathias.deremer-accetone@ineat-conseil.fr>
2020-10-08 14:29:41 +0200

    feat(config): override ns-config with 'config' env var

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
 import json
-from datetime import datetime, timedelta, timezone
 import os
-import time
-from ast import literal_eval
 import yaml
+from datetime import datetime, timedelta, timezone
+from ast import literal_eval
+from dotenv import load_dotenv
+from deepmerge import Merger
 
-# Load config file
+# Load config
 #with open("config/ns-killer.conf", 'r') as ymlfile:
 with open("/etc/config/ns-killer.conf", 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-# Fulfill config params with default values
+load_dotenv()
+cfg_from_env_var = os.environ.get("config")
+if(cfg_from_env_var != None):
+    merger = Merger(
+        [
+            (list, ["append"]),
+            (dict, ["merge"])
+        ],
+        ["override"],
+        ["override"]
+    )
+    cfg = merger.merge(cfg, json.loads(cfg_from_env_var))
 
 # Load cluster namespaces
 #with open('namespaces.json', 'r') as outfile:
@@ -35,17 +47,14 @@ def delete_ns(ns_name, ns_creationTimestamp_date):
     time_divider_unit = time_divider_units.get(cfg['config']['retention']['kind'])
     ns_creation_ago_time = divmod(ns_creation_ago.days * 86400 + ns_creation_ago.seconds, time_divider_unit)[0]
     if ns_creation_ago_time >= cfg['config']['retention']['time']:
-        #os.system("kubectl delete namespace {}".format(ns_name))
         print("{} | Killed namespace '{}' that lived for {} {}".format(date_now, ns_name, ns_creation_ago_time, cfg['config']['retention']['kind']))
 
-
 # Iterate on items
-print("Start iterating on namespaces")
+'''print("Start iterating on namespaces")
 for i in k8s_ns.get('items'):
     ns_name = i.get('metadata').get('name')
     ns_creationTimestamp = i.get('metadata').get('creationTimestamp')
     ns_creationTimestamp_date = datetime.strptime(ns_creationTimestamp, "%Y-%m-%dT%H:%M:%S%z")
-
     if  len(cfg['namespace']['only']) == 0:
       # Test if namespace is in exclude list
       if ns_name not in cfg['namespace']['exclude']:
@@ -53,4 +62,5 @@ for i in k8s_ns.get('items'):
     else:
         if ns_name in cfg['namespace']['only']:
           delete_ns(ns_name, ns_creationTimestamp_date)
+          '''
 
